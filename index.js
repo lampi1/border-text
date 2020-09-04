1120160c37214cf101eb49cd06196e3966cbde62
developer <info@bluemilk.it>
2020-09-04 16:24:58 +0200

    first commit

--- index.js
+++ index.js
@@ -5,12 +5,5 @@ function bordertext(width, strokeColor, textColor) {
 		el.style.color = textColor;
 	});
 }
-bordertext("1.5px", "blue", "transparent");
 
 module.exports.bordertext = bordertext;
-// .bordertitle{
-// 	-webkit-text-stroke-width: 1.5px;
-// 	-webkit-text-stroke-color: #763c52;
-// 	font-size: 123px;
-//   	color: transparent;
-// }
